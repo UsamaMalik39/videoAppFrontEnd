f370360c375e7c6f4b6bf0b4e65a30487a8897d2
Malik39 <usama.faisal@daoproptech.com>
2022-12-18 02:41:16 +0500

    videoAppFrontEnd

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'writesFrontENd'`, () => {
+  it(`should have as title 'videoAppFrontEnd'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('writesFrontENd');
+    expect(app.title).toEqual('videoAppFrontEnd');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('writesFrontENd app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('videoAppFrontEnd app is running!');
   });
 });
